cafba5d4723bcf6ac46327ecd906d832073bef7c
Joey2005 <joey2005@Joey2005s-Desktop.local>
2016-08-15 11:29:50 -0500

    foo

--- Algorithms/fft.cpp
+++ Algorithms/fft.cpp
@@ -6,69 +6,117 @@ using namespace std;
 const double pi=acos(-1.0); 
 const int maxn=(1<<16)+5; 
 struct Complex{
-    double a,b;  
+    double re,im;
     Complex(){}; 
-    Complex(double _a,double _b) {a=_a;b=_b;} 
+    Complex(double _re,double _im) {re=_re;im=_im;} 
 }; 
-int fft_len,id; 
+int n,id; 
 Complex tmp[maxn],P[maxn],PB[maxn]; 
-void fill(int s[],int L,int m,int d) { 
-    if (m==fft_len)
-        P[d]=Complex(id==L?0:s[id++],0); 
-    else { 
-        fill(s,L,m*2,d); 
-        fill(s,L,m*2,d+m); 
-    } 
-} 
-void fill2(int m,int d) { 
-    if (m==fft_len) 
-        P[d]=tmp[id++]; 
-    else { 
-        fill2(m*2,d); 
-        fill2(m*2,d+m); 
-    } 
-} 
-void fft(int oper) { 
-    for (int d=0;(1<<d)<fft_len;++d) { 
-        int i,m=(1<<d); 
-        double p0=2*pi/(double)(m*2)*(double)(oper); 
+void fft(int s[], int L, int oper) { 
+    int h = __builtin_ctz(n);
+    for (int i = 0; i < L; ++i) P[i] = Complex(s[i],0);
+    for (int i = L; i < n; ++i) P[i] = Complex(0,0);
+    for (int i = 0; i < n; ++i) {
+        int rev = 0;
+        for (int j = 0; j < h; ++j) {
+            rev = (rev << 1) | (i >> j & 1);
+        }
+        if (i < rev) {
+            swap(P[i], P[rev]);
+        }
+    }
+
+    for (int s = 2; s <= n; s <<= 1) {
+        int nt = s >> 1;
+        double p0=2*pi/(double)(s)*(double)(oper); 
         double sinp0=sin(p0); 
         double cosp0=cos(p0); 
-        for (i=0;i<fft_len;i+=(m*2)) { 
+        for (int j = 0; j < n; j += s) {
             double sinp=0, cosp=1; 
-            for (int j=0;j<m;j++) { 
-                double ta=cosp*P[i+j+m].a-sinp*P[i+j+m].b; 
-                double tb=cosp*P[i+j+m].b+sinp*P[i+j+m].a; 
-                P[i+j+m].a=P[i+j].a-ta; 
-                P[i+j+m].b=P[i+j].b-tb; 
-                P[i+j].a+=ta; 
-                P[i+j].b+=tb;   
+            for (int t = j; t < j + nt; ++t) {
+                int jp = t + nt;
+                double re = cosp * P[jp].re - sinp * P[jp].im; 
+                double im = cosp * P[jp].im + sinp * P[jp].re; 
+                P[jp].re = P[t].re - re; 
+                P[jp].im = P[t].im - im; 
+                P[t].re += re; 
+                P[t].im += im;  
                 double tsinp=sinp; 
                 sinp=sinp*cosp0+ cosp*sinp0; 
                 cosp=cosp*cosp0-tsinp*sinp0;   
             } 
         } 
     } 
+
+    if (oper == -1) {
+        for (int i = 0; i < n; ++i) {
+            P[i].re /= n;
+            P[i].im /= n;
+        }
+    }
 } 
-void convolute(int s1[], int L1, int s2[], int L2, int result[]) {
-    for (fft_len=1;fft_len<L1+L2;fft_len<<=1);
-    id=0; fill(s1,L1,1,0); fft(1); 
-    for (int i = 0; i < fft_len; ++i) PB[i]=P[i]; 
-    id=0; fill(s2,L2,1,0); fft(1); 
-    for (int i = 0; i < fft_len; ++i) { 
-        tmp[i].a=P[i].a*PB[i].a-P[i].b*PB[i].b; 
-        tmp[i].b=P[i].a*PB[i].b+P[i].b*PB[i].a; 
+void fft(int oper) { 
+    int h = __builtin_ctz(n);
+    for (int i = 0; i < n; ++i) {
+        int rev = 0;
+        for (int j = 0; j < h; ++j) {
+            rev = (rev << 1) | (i >> j & 1);
+        }
+        if (i < rev) {
+            swap(P[i], P[rev]);
+        }
+    }
+
+    for (int s = 2; s <= n; s <<= 1) {
+        int nt = s >> 1;
+        double p0=2*pi/(double)(s)*(double)(oper); 
+        double sinp0=sin(p0); 
+        double cosp0=cos(p0); 
+        for (int j = 0; j < n; j += s) {
+            double sinp=0, cosp=1; 
+            for (int t = j; t < j + nt; ++t) {
+                int jp = t + nt;
+                double re = cosp * P[jp].re - sinp * P[jp].im; 
+                double im = cosp * P[jp].im + sinp * P[jp].re; 
+                P[jp].re = P[t].re - re; 
+                P[jp].im = P[t].im - im; 
+                P[t].re += re; 
+                P[t].im += im;  
+                double tsinp=sinp; 
+                sinp=sinp*cosp0+ cosp*sinp0; 
+                cosp=cosp*cosp0-tsinp*sinp0;   
+            } 
+        } 
     } 
-    id=0; fill2(1,0); fft(-1); 
-    for (int i = 0; i < fft_len; ++i) {
-        result[i]=(int)(P[i].a/(double)fft_len+0.5);
+
+    if (oper == -1) {
+        for (int i = 0; i < n; ++i) {
+            P[i].re /= n;
+            P[i].im /= n;
+        }
+    }
+}
+void convolute(int s1[], int L1, int s2[], int L2, int result[]) {
+    for (n = 1; n < L1 + L2; n <<= 1);
+    fft(s1, L1, 1); 
+    for (int i = 0; i < n; ++i) PB[i] = P[i]; 
+    fft(s2, L2, 1); 
+    for (int i = 0; i < n; ++i) { 
+        tmp[i].re = P[i].re * PB[i].re - P[i].im * PB[i].im; 
+        tmp[i].im = P[i].re * PB[i].im + P[i].im * PB[i].re; 
+    } 
+    for (int i = 0; i < n; ++i) P[i] = tmp[i]; 
+    fft(-1); 
+    for (int i = 0; i < n; ++i) {
+        result[i] = (int)(P[i].re + 0.5);
     }
 }
 
 // random test
-int len = 30000, a[maxn], b[maxn], res[maxn];
+int a[maxn], b[maxn], res[maxn];
 
 int main() {
+    const int len = 30000;
     for (int _ = 0; _ < 10; ++_) {
         for (int i = 0; i < len; ++i) {
             a[i] = rand() % 10;
@@ -87,5 +135,6 @@ int main() {
             }
         }
         cout << _ << ": ok" << endl;
+        cerr << fixed << setprecision(10) << "time elpased = " << clock() / (double)(CLOCKS_PER_SEC) << " seconds." << endl;
     }
 }
