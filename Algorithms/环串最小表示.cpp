d5667885d09712073bbee7a088c9ac5385a7634c
Yan Liu <joey2005@Joey2005-MacBook-Pro.local>
2016-08-18 03:46:13 -0500

    foo

--- Algorithms/环串最小表示.cpp
+++ Algorithms/环串最小表示.cpp
@@ -3,20 +3,20 @@
 using namespace std;
 
 string getSmallest(string _s) {
-	int L = _s.size(), i, j, k, l;
+    int L = _s.size(), i, j, k, l;
     string s = _s + _s;
-	for (i = 0, j = 1; j < L; ) {
-		for (k = 0; k < L && s[i + k] == s[j + k]; k ++);
-		if (k >= L) break;
-		if (s[i + k] < s[j + k]) {
-			j += k + 1;
+    for (i = 0, j = 1; j < L; ) {
+        for (k = 0; k < L && s[i + k] == s[j + k]; k ++);
+        if (k >= L) break;
+        if (s[i + k] < s[j + k]) {
+            j += k + 1;
         } else {
-			l = i + k;
-			i = j;
-			j = max(l, j) + 1;
-		}
-	}
-	return s.substr(i, L);
+            l = i + k;
+            i = j;
+            j = max(l, j) + 1;
+        }
+    }
+    return s.substr(i, L);
 }
 
 int main() {
