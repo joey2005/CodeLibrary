a43c7c2b30d3fe864d88a028631e3558404f1fc1
joey2005 <joey2005.yali@gmail.com>
2022-02-16 20:05:07 -0500

    Update C.cpp

--- Contests/SNCKQL16/C.cpp
+++ Contests/SNCKQL16/C.cpp
@@ -2,27 +2,6 @@
 
 using namespace std;
 
-/*
-const int N = 1 << 16;
-
-int dp[N];
-
-int sg(int n) {
-    if (n == 0) return 0;
-    if (dp[n] != -1) return dp[n];
-    vector<bool> vis(6, false);
-    for (int i = 2; i <= 6; ++i) {
-        int ret = sg(n / i);
-        if (ret < 6) vis[ret] = true;
-    }
-    for (int i = 0; i < 6; ++i) {
-        if (!vis[i]) {
-            return dp[n] = i;
-        }
-    }
-}
-*/
-
 const int mul[] = {2, 2, 1, 3};
 
 int calcsg(long long n) {
@@ -38,21 +17,6 @@ int calcsg(long long n) {
 }
 
 int main() {
-    /*
-    memset(dp, -1, sizeof dp);
-    int lastsg = -1, cnt = 0;
-    for (int n = 1; n < N; ++n) {
-        if (sg(n) != sg(n - 1)) {
-            cout << lastsg << ' ' << cnt << endl;
-            cnt = 1;
-            lastsg = sg(n);
-        } else {
-            cnt++;
-        }
-    }
-    cout << lastsg << ' ' << cnt << endl;
-    */
-    //calcsg(1000000000000000000LL);
     int testCount;
     cin >> testCount;
     for (int testId = 1; testId <= testCount; ++testId) {
@@ -67,4 +31,3 @@ int main() {
         puts(ret ? "Henry" : "Derek");
     }
 }
-
